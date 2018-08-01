97623b5e7490c23e9a2a0cbd3401145783d01334
hyfree <wangxianqiangcn@foxmail.com>
2018-08-01 12:04:36 +0800

    添加项目文件。

---
+++ ume/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace ume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(365, 532);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "分    析";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(161)))), ((int)(((byte)(233)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(48, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(895, 404);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "\r\n";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1002, 600);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "U+mi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ume/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void time()
+        {
+            var t = DateTime.Now.AddMilliseconds(100);
+            while (DateTime.Now < t) Application.DoEvents();
+        }
+        public void files()
+        {
+            string path = @"C:\Users\Administrator\AppData\Local\Temp";
+            string[]t=Directory.GetFiles(path);
+            foreach (string i in t)
+            {
+                // MessageBox.Show(i);
+                //MessageBox.Show(i);
+              string cook = @"\S\.\S";
+              bool b = Regex.IsMatch(i,cook);
+                if (b)
+                {
+                    
+                   // MessageBox.Show(i + "=yes");
+                    FileInfo f = new FileInfo(i);
+        
+                    int l=int.Parse(f.Length.ToString());
+                    float ls = l / 1046529; //ls计算文件大小               
+                   // MessageBox.Show(ls.ToString()+".mb");
+                    try
+                    {
+                        File.Delete(i);
+                    }
+                    catch
+                    {
+                    }
+                }
+                else
+                {
+                   // MessageBox.Show(i+"=no");
+                }
+             // MessageBox.Show(i);
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 支持界面拖动
+        /// </summary>
+        const int HTLEFT = 10;
+        const int HTRIGHT = 11;
+        const int HTTOP = 12;
+        const int HTTOPLEFT = 13;
+        const int HTTOPRIGHT = 14;
+        const int HTBOTTOM = 15;
+        const int HTBOTTOMLEFT = 0x10;
+        const int HTBOTTOMRIGHT = 17;
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x0084:
+                    base.WndProc(ref m);
+                    Point vPoint = new Point((int)m.LParam & 0xFFFF,
+                        (int)m.LParam >> 16 & 0xFFFF);
+                    vPoint = PointToClient(vPoint);
+                    if (vPoint.X <= 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)HTTOPLEFT;
+                        else if (vPoint.Y >= ClientSize.Height - 5)
+                            m.Result = (IntPtr)HTBOTTOMLEFT;
+                        else m.Result = (IntPtr)HTLEFT;
+                    else if (vPoint.X >= ClientSize.Width - 5)
+                        if (vPoint.Y <= 5)
+                            m.Result = (IntPtr)HTTOPRIGHT;
+                        else if (vPoint.Y >= ClientSize.Height - 5)
+                            m.Result = (IntPtr)HTBOTTOMRIGHT;
+                        else m.Result = (IntPtr)HTRIGHT;
+                    else if (vPoint.Y <= 5)
+                        m.Result = (IntPtr)HTTOP;
+                    else if (vPoint.Y >= ClientSize.Height - 5)
+                        m.Result = (IntPtr)HTBOTTOM;
+                    break;
+                case 0x0201://鼠标左键按下的消息   
+                    m.Msg = 0x00A1;//更改消息为非客户区按下鼠标   
+                    m.LParam = IntPtr.Zero;//默认值   
+                    m.WParam = new IntPtr(2);//鼠标放在标题栏内   
+                    base.WndProc(ref m);
+                    break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+        public string linshi = "";
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        int i=0;
+        int f = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            if (i<=0)
+            {
+                i++;
+                textBox1.Text = "";
+                button1.Text = "分 析 中";
+                button1.Enabled = false;
+              
+              
+                if (f == 1)
+                {
+                    textBox1.Text += "视频音乐垃圾=0MB。\r\n"; time();
+                    textBox1.Text += "深度垃圾清理=0MB。\r\n";
+                    time();
+                    textBox1.Text += "网络垃圾清理=0MB。\r\n";
+                    time();
+                    textBox1.Text += "流氓软件清理=0MB。\r\n"; time();
+
+                    textBox1.Text += "其他软件垃圾=0MB。\r\n"; time();
+                    textBox1.Text += "社交软件垃圾=0MB。\r\n"; time();
+                    textBox1.Text += "系统产生的垃圾=0MB。\r\n"; time();
+                    textBox1.Text += "浏览器垃圾=0MB。\r\n"; time();
+                    textBox1.Text += "注册表垃圾=0项。\r\n"; time();
+                }
+                else
+                {
+                    textBox1.Text += "视频音乐垃圾=37MB。\r\n"; time();
+                    textBox1.Text += "深度垃圾清理=1.6GB。\r\n";
+                    time();
+                    textBox1.Text += "网络垃圾清理=73MB。\r\n";
+                    time();
+                    textBox1.Text += "流氓软件清理=1项。\r\n"; time();
+
+                    textBox1.Text += "其他软件垃圾=50MB。\r\n"; time();
+                    textBox1.Text += "社交软件垃圾=400kb。\r\n"; time();
+                    textBox1.Text += "系统产生的垃圾=66.9MB。\r\n"; time();
+                    textBox1.Text += "浏览器垃圾=103MB。\r\n"; time();
+                    textBox1.Text += "注册表垃圾=6项。\r\n"; time();
+                }
+                textBox1.Text += "分析完成。";
+                button1.Text = "清    理";
+                button1.Enabled = true;
+               
+
+
+
+            }
+
+            else
+            {
+
+                i = 0;
+                f = 1;
+                files();
+                textBox1.Text = "你的电脑已经非常干净。";
+                button1.Text = "分    析";
+
+               
+
+            }
+
+
+        }
+        /// <summary>
+        /// 退出程序
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+    }
+}
